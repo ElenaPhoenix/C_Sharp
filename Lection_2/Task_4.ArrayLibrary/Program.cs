17633596f1973dcfd62bdaca137d6ae4da25520f
ElenaPhoenix <sglena@mail.ru>
2022-11-29 09:56:32 +0300

    added Lection_2\Task_4.ArrayLibrary

---
+++ Lection_2/Task_4.ArrayLibrary/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(int[] collection) // void -невозвратный метод (нельзя использовать return), FillArray - случайное множество
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++; // - то же index = index+1;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int ind = 0;
+    while (ind < count)
+    {
+        Console.WriteLine(col[ind]);
+        ind++;
+    }
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
